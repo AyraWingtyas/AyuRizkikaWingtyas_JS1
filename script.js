b18cb2a67cd2b65ad8829f28ceac7d09c144ba2c
AyraWingtyas <92987152+AyraWingtyas@users.noreply.github.com>
2023-10-09 20:48:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* if, else, nested if */
 let isOn = true;
 let isDaytime = false;
 
@@ -14,3 +15,55 @@ if (isOn && isDaytime) {
 } else {
   console.log("The lamp is turned off");
 }
+
+/* switch */
+let day = "Tuesday";
+
+switch (day) {
+  case "Monday":
+    console.log("It's Monday!");
+    break;
+  case "Tuesday":
+    console.log("It's Tuesday!");
+    break;
+  case "Wednesday":
+    console.log("It's Wednesday!");
+    break;
+  case "Thursday":
+    console.log("It's Thursday!");
+    break;
+  case "Friday":
+    console.log("It's Friday!");
+    break;
+  default:
+    console.log("It's the weekend!");
+}
+
+/* for statement */
+for (let i = 1; i < 5; i++) {
+  console.log("Iteration: " + i);
+}
+
+/* while, do while */
+let i = 0;
+
+while (i < 5) {
+  console.log("Iteration: " + i);
+  i++;
+}
+
+let o = 0;
+
+do {
+  console.log("Iteration: " + i);
+  i++;
+} while (i < 5);
+
+/* function */
+function greet(name) {
+  console.log("Hello, " + name + "!");
+}
+
+// Calling the function
+greet("Ayu");
+greet("Rkive");
